e45aa8769506deddc055b3de7c6168245f7f9ae0
Paula <paulatfortuna@gmail.com>
2021-04-13 12:51:05 +0100

    link portuguese

--- app.py
+++ app.py
@@ -247,7 +247,7 @@ app.layout = html.Div(children=[
                 id='contacto_description',
                 children= ['If you want to know more about this project or author, please visit:',
                            dcc.Markdown('''[GitHub](https://github.com/paulafortuna/feminicidioAvista) and [LinkedIn](https://pt.linkedin.com/in/paula-fortuna-a6b75a7a).'''),
-                            dcc.Markdown('''[PT](https://feminicidioavista.herokuapp.com/)'''),
+                            dcc.Markdown('''<center>[Portuguese Version](https://feminicidioavista.herokuapp.com/)</center>'''),
                            ]
             )
         ],
